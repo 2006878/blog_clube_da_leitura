c9ae977d8b596c39f3c76fcd5034b9fc6921adff
Tairone Amaral <2006878@aluno.univesp.br>
2022-04-11 07:44:47 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    summary = RichTextField(blank=True, null=True)
+    content = RichTextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
